400252efa93d3c0ccb49fe9943472db7085f089b
Vaibhav <vjdomadia@gmail.com>
2020-09-15 23:13:10 +0530

    Add new Github Icon to the Header

--- src/Components/Header/header.js
+++ src/Components/Header/header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './header.css'
+import GitHubIcon from '../../Icons/githubicon.svg';
 
 class Header extends React.Component {
     constructor(props) {
@@ -10,6 +11,7 @@ class Header extends React.Component {
         return (
             <div>
                 <h2>2048</h2>
+                <img src = {GitHubIcon} alt = "Github Link to the Project"></img>
             </div>
         );
     }
