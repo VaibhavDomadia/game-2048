7ed815711c382d201a10acee7aa34183c28d6c88
Vaibhav <vjdomadia@gmail.com>
2020-09-17 01:01:00 +0530

    Updates

---
+++ precache-manifest.072c448eba4f3553345265bb52909a4c.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a15d31a700de8accf3bfa0acdda2c41",
+    "url": "/game-2048/index.html"
+  },
+  {
+    "revision": "db2ab8ad32e3d364825f",
+    "url": "/game-2048/static/css/main.c04d032a.chunk.css"
+  },
+  {
+    "revision": "20e63af5ebbe69b0ed08",
+    "url": "/game-2048/static/js/2.60f9dead.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/game-2048/static/js/2.60f9dead.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db2ab8ad32e3d364825f",
+    "url": "/game-2048/static/js/main.e0f943d5.chunk.js"
+  },
+  {
+    "revision": "698f9bf3d8b533aa7d0e",
+    "url": "/game-2048/static/js/runtime-main.fc6511cb.js"
+  },
+  {
+    "revision": "0bda22ab2539261ee7eb04e7252349e1",
+    "url": "/game-2048/static/media/arrowdown.0bda22ab.svg"
+  },
+  {
+    "revision": "fc7c38a8b9f24e38fcbaf6dbefa0dd21",
+    "url": "/game-2048/static/media/arrowleft.fc7c38a8.svg"
+  },
+  {
+    "revision": "17866ce5bb23006fd1322e3e1799e945",
+    "url": "/game-2048/static/media/arrowright.17866ce5.svg"
+  },
+  {
+    "revision": "984a6c88fd45098d0a85fa8dd83a0f5b",
+    "url": "/game-2048/static/media/arrowup.984a6c88.svg"
+  },
+  {
+    "revision": "a2594cecc10060ea97781a70ee5125d5",
+    "url": "/game-2048/static/media/githubicon.a2594cec.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/game-2048/static/media/logo.5d5d9eef.svg"
+  }
+]);
