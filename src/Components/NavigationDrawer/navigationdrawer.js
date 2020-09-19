d0e64d4924a218a3c888d004c648b4f2cfc9224b
Vaibhav <vjdomadia@gmail.com>
2020-09-19 18:36:31 +0530

    Add Icons to Navigation Drawer and styles

--- src/Components/NavigationDrawer/navigationdrawer.js
+++ src/Components/NavigationDrawer/navigationdrawer.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import './navigationdrawer.css';
+import ClassicIcon from '../../Icons/classic.svg';
+import ReverseIcon from '../../Icons/reverse.svg';
+import Classic2048 from '../Classic2048/classic2048';
+import Reverse2048 from '../Reverse2048/reverse2048';
 
 class NavigationDrawer extends React.Component {
     constructor(props) {
@@ -14,14 +18,20 @@ class NavigationDrawer extends React.Component {
             style.left = "0%";
         }
         else {
-            style.left = "-20%";
+            style.left = "-25%";
         }
         
         return (
             <div className = "navigationDrawer" style = {style}>
                 <h4 className = "navigationItemGroupTitle">VARIANTS</h4>
-                <div className = {currentGameName == 'CLASSIC2048' ? 'navigationItem active' : 'navigationItem'} onClick = {() => changeCurrentGame('CLASSIC2048')}>Classic 2048</div>
-                <div className = {currentGameName == 'REVERSE2048' ? 'navigationItem active' : 'navigationItem'} onClick = {() => changeCurrentGame('REVERSE2048')}>Reverse 2048</div>
+                <div className = {currentGameName == 'CLASSIC2048' ? 'navigationItem active' : 'navigationItem'} onClick = {() => changeCurrentGame('CLASSIC2048')}>
+                    <img src = {ClassicIcon}></img>
+                    <div>Classic 2048</div>
+                </div>
+                <div className = {currentGameName == 'REVERSE2048' ? 'navigationItem active' : 'navigationItem'} onClick = {() => changeCurrentGame('REVERSE2048')}>
+                    <img src = {ReverseIcon}></img>
+                    <div>Reverse 2048</div>
+                </div>                
             </div>
         );
     }
