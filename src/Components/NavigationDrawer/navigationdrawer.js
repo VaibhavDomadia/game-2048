d47c1306f9d7feb69f3c9acce4bd049f423b9c5c
Vaibhav <vjdomadia@gmail.com>
2020-09-23 19:42:15 +0530

    Add icons and applying new theme

--- src/Components/NavigationDrawer/navigationdrawer.js
+++ src/Components/NavigationDrawer/navigationdrawer.js
@@ -2,6 +2,8 @@ import React from 'react';
 import './navigationdrawer.css';
 import ClassicIcon from '../../Icons/classic.svg';
 import ReverseIcon from '../../Icons/reverse.svg';
+import ClassicBlueIcon from '../../Icons/classicblue.svg';
+import ReverseBlueIcon from '../../Icons/reverseblue.svg';
 import Classic2048 from '../Classic2048/classic2048';
 import Reverse2048 from '../Reverse2048/reverse2048';
 
@@ -25,11 +27,11 @@ class NavigationDrawer extends React.Component {
             <div className = "navigationDrawer" style = {style}>
                 <h4 className = "navigationItemGroupTitle">VARIANTS</h4>
                 <div className = {currentGameName == 'CLASSIC2048' ? 'navigationItem active' : 'navigationItem'} onClick = {() => changeCurrentGame('CLASSIC2048')}>
-                    <img src = {ClassicIcon}></img>
+                    <img src = {currentGameName == 'CLASSIC2048' ? ClassicBlueIcon : ClassicIcon}></img>
                     <div>Classic 2048</div>
                 </div>
                 <div className = {currentGameName == 'REVERSE2048' ? 'navigationItem active' : 'navigationItem'} onClick = {() => changeCurrentGame('REVERSE2048')}>
-                    <img src = {ReverseIcon}></img>
+                    <img src = {currentGameName == 'REVERSE2048' ? ReverseBlueIcon : ReverseIcon}></img>
                     <div>Reverse 2048</div>
                 </div>                
             </div>
